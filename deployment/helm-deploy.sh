2c081552284f914eff83c0c91af9a18e36e3f926
Tomas Beke <tomas.beke@garwan.sk>
2024-10-10 12:40:48 +0200

    Add more env overrides for config, clean up Helm script

--- deployment/helm-deploy.sh
+++ deployment/helm-deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export DOCKER_REGISTRY_BASE=localhost:5000
 export PROJECT_NAME=home-monitoring
-helm upgrade --install -f ./helm/values-dev.yaml --wait --timeout 1m30s --set image.version=latest --set image.prefix=$DOCKER_REGISTRY_BASE/$PROJECT_NAME victron-mqtt-bridge ./helm
+helm upgrade --install -f ./helm/values-local.yaml --wait --timeout 1m30s --set image.version=latest --set image.prefix=$DOCKER_REGISTRY_BASE/$PROJECT_NAME victron-mqtt-bridge ./helm
